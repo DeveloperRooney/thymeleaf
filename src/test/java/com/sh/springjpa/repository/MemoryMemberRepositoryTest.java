d2aaa4c03c39d40040befe48abaa897004029c75
DeveloperRooney <han_0427@naver.com>
2021-07-19 13:46:47 +0900

    각 메소드의 의존관계와 상관없이 실행 가능한 테스트 클래스 생성

---
+++ src/test/java/com/sh/springjpa/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.sh.springjpa.repository;
+
+import com.sh.springjpa.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+
+        member.setName("Spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+
+        Assertions.assertThat(member).isEqualTo(result);
+
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("Spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("Spring1").get();
+
+        Assertions.assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member member3 = new Member();
+        member3.setName("spring3");
+        repository.save(member3);
+
+        Member member4 = new Member();
+        member4.setName("spring4");
+        repository.save(member4);
+
+        List<Member> result = repository.findAll();
+
+        Assertions.assertThat(result.size()).isEqualTo(4);
+    }
+}
