60c3fa02a4a8e0d28e844d75565adcae79a2524c
DeveloperRooney <han_0427@naver.com>
2021-07-21 00:27:38 +0900

    JDBC를 이용한 고전적인 리포지토리 생성

---
+++ src/main/java/com/sh/springjpa/repository/JDBCMemberRepository.java
@@ -0,0 +1,104 @@
+package com.sh.springjpa.repository;
+
+import com.sh.springjpa.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class JDBCMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    public JDBCMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+
+        String sql = "insert into member(name) values(?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+
+            conn = dataSource.getConnection();
+
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, member.getName());
+
+            pstmt.executeUpdate();
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                rs.close();
+                pstmt.close();
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+
+        String sql = "select * from member where id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }else {
+                return Optional.empty();
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                rs.close();
+                pstmt.close();
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
